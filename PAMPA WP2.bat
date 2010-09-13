052ae29e4bad7b5ac5a331a4a084f2b4a899c6c9
Yves Reecht <Yves.Reecht@marine.ie>
2010-09-13 15:56:39 +0000

    Version 0.92 : Ajout d'un .bat + fichiers utiles à l'installeur. Correction d'un possiblement bug bloquant (utilisation d'une sauvegarde globale de résultat de modèle linéaire plutôt que la version locale).
    git-svn-id: svn://localhost@14 8ebfeadf-b5f3-be42-b472-c96887378374

---
+++ PAMPA WP2.bat
@@ -0,0 +1,8 @@
+@echo off
+set valeur=reg query "HKEY_LOCAL_MACHINE\Software\R-core\R" /v InstallPath
+set Rpathstr=findstr /I /L /C:"REG_SZ"
+for /f "tokens=3 delims=	" %%i in ('%valeur%^|%Rpathstr%') do set Rpath="%%i"
+
+rem set R_PROFILE=Rprofile.site
+start "PAMPA WP2" %Rpath%\bin\Rgui.exe R_PROFILE=Exec/Rprofile.site --no-restore
+exit

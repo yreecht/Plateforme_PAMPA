d491732f044f9b4a6cfa81cfc43534a4bf80558a
Yves Reecht <Yves.Reecht@hi.no>
2020-06-05 12:27:07 +0200

    Fixes Linux startup bash for invocation from anywhere (set pwd)

--- Scripts_Biodiv/PAMPA_WP2.bash
+++ Scripts_Biodiv/PAMPA_WP2.bash
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+## Set the right path:
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ]
+then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  # exit 1  # fail
+  echo "Path issue"
+else
+  cd "$MY_PATH"
+fi
+
+## echo "$MY_PATH"
+
+## Go to parent directory of the script.
 pwd | egrep -q "Scripts_Biodiv$" && cd ..
 
-echo `pwd`
+## echo `pwd`
 
+## R_PROFILE which loads automatically the platform:
 export R_PROFILE="Scripts_Biodiv/Rprofile.site"
 
-## (R --no-restore --no-save)
-
-## R --file=./Scripts_Biodiv/Main_Unix.R --interactive --no-restore --no-save
-
-## R R_PROFILE=./Scripts_Biodiv/Rprofile.site --interactive --no-restore --no-save
-
+## Launch R:
 exec R --interactive --no-restore --no-save
-## source("./Scripts_Biodiv/Main.R", encoding="latin1")
 
-## (R --no-restore --no-save < ./Scripts_Biodiv/Main_Unix.R)
-
-## (Rscript ./Scripts_Biodiv/Main_Unix.R --no-restore --no-save --no-site-file  --interactive) # R_PROFILE=./Scripts_Biodiv/Rprofile.site)
-
-## wait
-
-## no-restore --no-save
+echo ""
